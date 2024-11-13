aee80f71d268039eebf21f24709cc2c30ce72ad9
CEN-s <cen.silva@unesp.br>
2024-11-13 17:22:58 -0300

    mudei bobeira

--- main.py
+++ main.py
@@ -27,24 +27,24 @@ t = transforms.Compose([
             ])
 
 if not os.path.exists("datasets/outex"):
-    data = create_folder_dataset("datasets originais/outex", "1200tex", 60)
+    data = create_folder_dataset("datasets originais/outex", "outex", 20)
 
-data = ImageFolder(root="datasets/1200tex", transform=t)
+data = ImageFolder(root="datasets/outex", transform=t)
 indices = rep.split_indices(data)
 rnn = RNN(Q=max_aug_factor, P=3*3)
 
 accuracies = []
 for i in range(1, max_aug_factor+1):
-    resnet = timm.create_model('resnet18', pretrained=False, in_chans=1, num_classes=20)
+    resnet = timm.create_model('resnet18', pretrained=False, in_chans=1, num_classes=68)
     resnet.to(device)
     accuracies.append(fit(rnn, resnet, data, device, indices, aug_factor=i,  num_epochs=100))
 
 accuracies = pd.DataFrame(accuracies)
-accuracies.to_csv('resultados/accuracy_1200tex.csv')
+accuracies.to_csv('resultados/accuracy_outex.csv')
 accuracies.columns = ['Accuracy']
 accuracies.index = range(1, len(accuracies) + 1)
 accuracies.plot()
 plt.xlabel('Augmentation Factor')
 plt.ylabel('Accuracy')
 plt.title('Model Accuracy vs Augmentation Factor')
-plt.savefig('resultados/accuracy_plot_1200tex.png')
+plt.savefig('resultados/accuracy_plot_outex.png')
